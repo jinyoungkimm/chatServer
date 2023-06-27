d9495fc631d53c6871877735023ff6a7abb71413
Jin Young Kim <wlsdud66523@kakao.com>
2023-06-27 14:44:24 +0900

    채팅방 기능 구현(완료)

--- src/main/java/com/cos/chatapp/CharController.java
+++ src/main/java/com/cos/chatapp/CharController.java
@@ -17,7 +17,8 @@ public class CharController {
 
     private final ChatRepository chatRepository;
 
-    @CrossOrigin
+    // 귓속말 할 때 사용!
+   /* @CrossOrigin
     // MediaType.TEXT_EVENT_STREAM_VALUE : SSE Protocl기능! ( 요청이 와서 응답을 보내도, 응답 channel은 끊지 않고 계속해서 응답 channel로 데이터를 보낼 수 있게 해줌)
     @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Chat> getMessage(@PathVariable String sender,@PathVariable String receiver){
@@ -25,7 +26,27 @@ public class CharController {
     return chatRepository.mFindBySender(sender,receiver)
             .subscribeOn(Schedulers.boundedElastic());
 
+    }*/
+
+
+    // 귓속말이 아니라, [채팅팅방] 능을 구현하겠다.
+    @CrossOrigin
+    @GetMapping(value="/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum){
+
+        return chatRepository.mFindByRoomNum(roomNum);
+
     }
+
+
+    @PostMapping("/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat) {
+
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat); // Spring Data가 자동으로 SAVE()를 이미 구현해 놓았다.
+
+    }
+
 
    /* @CrossOrigin
     @PostMapping("/chat")
@@ -36,13 +57,7 @@ public class CharController {
 
     }*/
 
-    @PostMapping("/chat")
-    public Mono<Chat> setMsg(@RequestBody Chat chat) {
 
-        chat.setCreatedAt(LocalDateTime.now());
-        return chatRepository.save(chat); // Spring Data가 자동으로 SAVE()를 이미 구현해 놓았다.
-
-    }
 
 
 }
--- src/main/java/com/cos/chatapp/ChatRepository.java
+++ src/main/java/com/cos/chatapp/ChatRepository.java
@@ -10,10 +10,10 @@ import reactor.core.publisher.Flux;
 
 public interface ChatRepository extends ReactiveMongoRepository<Chat,String> {
 
-
-    @Tailable // 커서를 안 닫고 계속 유지하게 한다.(티스토리 참조)
+    // 여기서 부터는 이제 메시지 송신자와 수신자 간에 다이렉트로 하는 귓속말 기능이 아니라, 송신자와 수신자가 [채팅방]을 통해서 채팅하는 기능을 구현을 할 것이다.
+   /* @Tailable // 커서를 안 닫고 계속 유지하게 한다.(티스토리 참조)
     @Query("{sender:?0, receiver:?1}") // db.chat.find({sender:'cos',receiver:'ssar'})와 같은 형태의 MongoDB의 SELECT 기능이다.
-    /**
+    *//**
      * sender == cos, receiver == ssar인 JSON Object를 chat 컬렉션에서 조회하는 기능능     * [
      *   {
      *     _id: ObjectId("6498a0d6e264048efbafafc3"),
@@ -22,11 +22,11 @@ public interface ChatRepository extends ReactiveMongoRepository<Chat,String> {
      *     msg: '응 반가워'
      *   }
      * ]
-     */
-    Flux<Chat> mFindBySender(String sender,String receiver); // Flux(흐름) : 연결을 유지하면서 데이터를 계속 흘려 보낼 수가 있다.
+     *//*
+    Flux<Chat> mFindBySender(String sender,String receiver); // Flux(흐름) : 연결을 유지하면서 데이터를 계속 흘려 보낼 수가 있다.*/
 
 
-
+    // 더이상 귓속말 기능이 아니라, [채팅방]을 매개체로 송/수신자가 대화를 할 것이며, 채팅방 번호에 해당하는 채팅 내용(Chat Collection)을 MongoDB에서 가져 올 것이다.
     @Tailable
     @Query("{roomNum:?0}")
     Flux<Chat> mFindByRoomNum(Integer roomNum);
